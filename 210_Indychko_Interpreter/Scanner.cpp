d44b04ff45b4b2c1a811a4f7909eb759533cd55d
luseno4ek <olindyv@mail.ru>
2021-04-10 23:45:45 +0300

    finished scanner

--- 210_Indychko_Interpreter/Scanner.cpp
+++ 210_Indychko_Interpreter/Scanner.cpp
@@ -1,1 +1,29 @@
 #include "Scanner.hpp"
+#include <cstring>
+
+Scanner::Scanner(const char* program) {
+    fp = fopen(program, "r"); // file "program" is opened for reading
+    curr_state = INIT;
+    clear();
+    gc();
+}
+
+void Scanner::clear() {
+    buf_top = 0;
+    for(int i = 0; i < 80; i++) { buf[i] = '\0'; }
+}
+
+void Scanner::add() { buf[buf_top++] = c; }
+
+int Scanner::look(const char *_buf, char **list) {
+    int i = 0;
+    while(list[i] != NULL) {
+        if(strcmp(buf, list[i]) == 0) { return i; }
+        i++;
+    }
+    return 0;
+}
+
+void Scanner::gc() { c = fgetc(fp); }
+
+
--- 210_Indychko_Interpreter/Scanner.hpp
+++ 210_Indychko_Interpreter/Scanner.hpp
@@ -1,7 +1,7 @@
 #ifndef Scanner_hpp
 #define Scanner_hpp
 
-#include "Lexeme.hpp"
+#include "Identifier.hpp"
 #include <stdio.h>
 
 class Scanner {
@@ -15,24 +15,111 @@ private:
         DELIM,
         NEQ
     };
-    static char* ServiceWords[];
-    static char* LimSymbols[];
+    static const char* ServiceWords[];
+    static const char* LimSymbols[];
     static TypeOfLex words[];
-    static TypeOfLex dlms[];
+    static TypeOfLex symbols[];
     State curr_state;
     FILE* fp;
     char c;
-    char buf[20];
+    char buf[80];
     int buf_top;
+    ///clears the buffer
     void clear();
+    ///adds char c to the top of buffer
     void add();
+    ///looks if _buf is in list and returns its position (or 0)
     int look(const char* _buf, char** list);
+    ///gets char c from FILE* fp
     void gc();
 public:
-    Lex get_lex();
     Scanner(const char* program);
+    Lex get_lex();
 };
 
+/*/////////////////////////////////////////*/
 
+const char* Scanner::ServiceWords[] = {
+    ""
+    "program",
+    "int",
+    "string",
+    "if",
+    "else",
+    "while",
+    "do",
+    "false",
+    "true",
+    "read",
+    "write",
+    "not",
+    "and",
+    "or",
+    "continue",
+    "boolean",
+    NULL
+};
+
+/*/////////////////////////////////////////*/
+
+const char* Scanner::LimSymbols[] = {
+    ""
+    "*",
+    "/",
+    "+",
+    "-",
+    "<",
+    ">",
+    "<=",
+    ">=",
+    "==",
+    "!=",
+    "=",
+    ";",
+    ",",
+    ":",
+    "(",
+    ")",
+    NULL
+};
+
+/*/////////////////////////////////////////*/
+
+TableIdent Identifiers(100);
+
+/*/////////////////////////////////////////*/
+
+TypeOfLex Scanner::words[] = {
+    LEX_NULL,
+    LEX_PROGRAM,
+    LEX_END, LEX_BEGIN,
+    LEX_STRING, LEX_INT,
+    LEX_IF, LEX_THEN, LEX_ELSE,
+    LEX_DO, LEX_WHILE,
+    LEX_FALSE, LEX_TRUE,
+    LEX_NOT, LEX_OR, LEX_AND,
+    LEX_READ, LEX_WRITE,
+    LEX_NULL
+};
+
+/*/////////////////////////////////////////*/
+
+TypeOfLex Scanner::symbols[] = {
+    LEX_FIN,
+    LEX_SEMICOLON, // ;
+    LEX_COMMA,     // ,
+    LEX_COLON,     // :
+  //  LEX_ASSIGN,
+  //  LEX_LPAREN,
+  //  LEX_RPAREN,
+  //  LEX_EQ,
+  //  LEX_LSS, LEX_GTR,
+    LEX_PLUS, LEX_MINUS,
+  //  LEX_TIMES,
+  //  LEX_SLASH, LEX_LEQ,
+    LEX_NEQ,
+  //  LEX_GEQ,
+    LEX_NULL
+};
 
 #endif /* Scanner_hpp */
